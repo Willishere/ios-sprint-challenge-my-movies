50110b708cab24e7db047f75d8d5b98302c5b93c
William Chen <chen.will7@gmail.com>
2019-09-20 09:05:17 -0700

    finished Movie Controller

--- MyMovies/Model Controllers/MovieController.swift
+++ MyMovies/Model Controllers/MovieController.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreData
 
 class MovieController {
     
@@ -58,14 +59,79 @@ class MovieController {
         }.resume()
     }
     
-
-    func createMovie(with title: String, identifier: UUID , mood: String) {
+    
+    func createMovie(with title: String, hasWatched: Bool) {
         
-        let
+        let movie = Movie(title: title, hasWatched: hasWatched)
         
-        put(entry: entry)
+        put(movie: movie)
         
         CoreDataStack.shared.save()
+    }
+    
+    func update(movie: Movie, title: String) {
+        
+        movie.title = title
+        
+        put(movie: movie)
+        
+        CoreDataStack.shared.save()
+    }
+    
+    func delete(movie: Movie) {
+        
+        CoreDataStack.shared.mainContext.delete(movie)
+        deleteMovieFromServer(movie: movie)
+        CoreDataStack.shared.save()
+    }
+    
+    private func put(movie: Movie, completion: @escaping ((Error?) -> Void) = { _ in }) {
+        
+        let identifier = movie.identifier ?? UUID().uuidString
+        let requestURL = baseURL.appendingPathComponent(identifier).appendingPathExtension("json")
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "PUT"
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(movie.movieRepresentation)
+        } catch {
+            NSLog("Error encoding Entry: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("Error PUTting Entry to server: \(error)")
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+            }.resume()
+    }
+    
+    func deleteMovieFromServer(movie: Movie, completion: @escaping ((Error?) -> Void) = { _ in }) {
+        
+        guard let identifier = movie.identifier else {
+            NSLog("Entry identifier is nil")
+            completion(NSError())
+            return
+        }
+        
+        let requestURL = baseURL.appendingPathComponent(identifier).appendingPathExtension("json")
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "DELETE"
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("Error deleting entry from server: \(error)")
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+            }.resume()
     }
     
     func fetchMoviesFromServer(completion: @escaping ((Error?) -> Void) = { _ in }) {
@@ -90,7 +156,7 @@ class MovieController {
             
             do {
                 searchedMovies = try JSONDecoder().decode([String: MovieRepresentation].self, from: data).map({$0.value})
-                self.updateEntries(with: searchedMovies)
+                self.updateMovies(with: searchedMovies)
             } catch {
                 NSLog("Error decoding JSON data: \(error)")
                 completion(error)
@@ -100,6 +166,53 @@ class MovieController {
             completion(nil)
             }.resume()
         
+    }
+    
+    private func updateMovies(with representations: [MovieRepresentation]) {
+        
+        let moviesWithID = representations.filter({ $0.identifier != nil })
+        let identifiersToFetch = moviesWithID.compactMap({ UUID(uuidString: $0.identifier!) })
+        
+        let representationsByID = Dictionary(uniqueKeysWithValues: zip(identifiersToFetch, moviesWithID))
+        
+        var moviesToCreate = representationsByID
+        
+        let fetchRequest: NSFetchRequest<Movie> = Movie.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "identifier IN %@", identifiersToFetch)
+        
+        let context = CoreDataStack.shared.container.newBackgroundContext()
+        
+        context.performAndWait {
+            
+            do {
+                let existingMovies = try context.fetch(fetchRequest)
+                
+                for movie in existingMovies {
+                    guard let id = movie.identifier,
+                        let identifier = UUID(uuidString: id),
+                        let representation = representationsByID[identifier] else { continue }
+                    self.update(movie: movie, with: representation)
+                    
+                    moviesToCreate.removeValue(forKey: identifier)
+                }
+                
+                for representation in moviesToCreate.values {
+                    Movie(movieRepresentation: representation, context: context)
+                }
+                
+            } catch {
+                NSLog("Error fetching tasks for UUIDs: \(error)")
+            }
+            
+            CoreDataStack.shared.save(context: context)
+        }
+    }
+    
+    
+    private func update(movie: Movie, with movieRep: MovieRepresentation) {
+        movie.title = movieRep.title
+        movie.hasWatched = movieRep.hasWatched ?? false
+        movie.identifier = movieRep.identifier
     }
     
     // MARK: - Properties
